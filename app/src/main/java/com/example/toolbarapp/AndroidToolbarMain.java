854495c21f6cb335c976d250b6913496b2dc1013
Kamila <kamila.sroda@vp.pl>
2016-12-03 15:59:24 +0100

    init

---
+++ app/src/main/java/com/example/toolbarapp/AndroidToolbarMain.java
@@ -0,0 +1,60 @@
+package com.example.toolbarapp;
+
+/**
+ * Created by Kamila on 01.12.2016.
+ */
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.toolbarapp.androidtoolbarexample.R;
+
+public class AndroidToolbarMain extends AppCompatActivity {
+
+    Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.first_activity);
+        initToolBar();
+
+    }
+    // Menu icons are inflated just as they were with actionbar
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    public void initToolBar() {
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle( R.string.toolbarTitle2);
+
+        setSupportActionBar(toolbar);
+
+        /*toolbar.setNavigationIcon(R.drawable.ic_toolbar_arrow);
+        toolbar.setNavigationOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Toast.makeText(AndroidToolbarMain.this, "clicking the toolbar!", Toast.LENGTH_SHORT).show();
+                    }
+                }
+
+        );*/
+    }
+
+    public void sendMessage(View view)
+    {
+        Intent intent = new Intent(AndroidToolbarMain.this, AndroidToolbarExample.class);
+        startActivity(intent);
+    }
+}
